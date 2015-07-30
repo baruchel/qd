a4f27c4dcd7a81c7a6d063ab6e7e279d27c6a9bb
Thomas Baruchel <baruchel@gmx.com>
2015-07-30 22:56:06 +0200

    Fixed version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension
 setup(
     name = 'QD Interface',
     description = 'A wrapper for the QD library.',
-    version = '0.1 alpha',
+    version = '0.1.1 alpha',
     ext_modules = [ Extension('qd', ['qd.c'], libraries=['qd'] ), ],
     url='http://baruchel.hd.free.fr/',
     author='Thomas Baruchel',
